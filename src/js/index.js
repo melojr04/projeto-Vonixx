e800856c892137e245a67bff901a2bad73cdadb3
José Luiz de Lima Melo Junior <melojr1910@gmail.com>
2025-01-28 09:00:37 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,20 @@ const btnVoltar = document.getElementById("btn-voltar");
 const produtos = document.querySelectorAll(".produto");
 let produtoAtual = 0;
 
+produtos.forEach((produto) =>{
+  produto.addEventListener("click", function(){
+   const produtoVirado = produto.querySelector(".produto-virado");
+
+   //virar o cartão
+   produto.classList.toggle("virar");
+   //mostrar o fundo da carta
+    produtoVirado.classList.toggle("mostrar-fundo-produto");
+
+    const descricao = produto.querySelector(".descricao");
+    descricao.classList.toggle("esconder");
+  });  
+});
+
 btnAvancar.addEventListener("click", function () {
   if (produtoAtual === produtos.length - 1) return;
 
@@ -30,3 +44,4 @@ function esconderProdutoSelecionado() {
   const produtoSelecionado = document.querySelector(".selecionado");
   produtoSelecionado.classList.remove("selecionado");
 }
+
